d9ba1300e6572e6e9b7dc352b3dd6ea8ed4ba276
Johan Mabille <johan.mabille@gmail.com>
2021-02-03 21:45:36 +0100

    Reviewed InterestRate

--- InterestRates.h
+++ InterestRates.h
@@ -9,6 +9,11 @@ class InterestRates{
         virtual double getMeanIR() = 0;
         virtual InterestRates* bumped(double bump) = 0;
         virtual ~InterestRates();
+        // ENtity semantics:
+        InterestRates(const InterestRates&) = delete;
+        InterestRates& operator=(const InterestRates&) = delete;
+        InterestRates(InterestRates&&) = delete;
+        InterestRates& operator=(InterestRates&&) = delete;
 
     protected:
 
@@ -30,6 +35,8 @@ class GeneralIR: public InterestRates{
     public:
         explicit GeneralIR(double irs[], int points_available);
         explicit GeneralIR(double irs[], int points_available, int points_grid);
+        // Would be better to pass them by constant reference to avoid
+        // useless copies
         explicit GeneralIR(std::vector<double> irs);
         explicit GeneralIR(std::vector<double> irs, int points_grid);
         double getIRAt(int j);
@@ -38,6 +45,7 @@ class GeneralIR: public InterestRates{
         virtual ~GeneralIR();
 
     protected:
+        // Wy not using std::vector?
         double* m_irs;
         int m_size;
         int m_points_grid;
