de2887c0d4385a7dc0f133f7b461f63f4d5310bc
Johan Mabille <johan.mabille@gmail.com>
2021-02-03 21:40:33 +0100

    Review Matrix

--- Matrix.h
+++ Matrix.h
@@ -14,6 +14,10 @@ class Matrix
         std::string to_string() const;
 
         T elem_at(int i, int j) const;
+        // Why returning a pointer to the object?
+        // You could return a reference if you want to use
+        // chained methods, but that's unlikely for classes
+        // like this
         Matrix* set_elem_at(int i, int j, T val);
 
         Matrix extract_column(int j) const;
@@ -29,11 +33,20 @@ class Matrix
         Matrix operator+(const T &scal) const;
         Matrix& operator=(const Matrix &cop);
 
+        // Why a virtual destructor? This class
+        // has value semantics and should not be
+        // inherited
         virtual ~Matrix();
 
     protected:
         int m_rows;
         int m_cols;
+        // Why not std::vector?
+        // You didn't implement copy constructor,
+        // not move constructor and move assign operators
+        // Having a pointer here without the right value
+        // semantics implementation epxoses you to a lot
+        // of potential crashes.
         T* m_data;
 
         void inverse_row(Matrix* M, int row, int col);
